16452fbc471ddee2db4894cd8f08de7f5d5bdf0c
bukanspot <wicaksana0701@gmail.com>
2024-08-26 19:38:16 +0800

    support multiple directory

--- bascp.sh
+++ bascp.sh
@@ -7,23 +7,30 @@ if [ -z $1  ]; then
     exit 1
 fi
 
-cd $1
+for arg in "$@"; do
+    if [ -z "$arg" ]; then
+        break
+    fi
 
-if [ ! -f .env ]; then
-    echo -e "\033[0;31m\nPlease create .env file\033[0m\n"
-    echo "See https://github.com/spotikum/bascp/blob/master/.env.example"
+    cd $arg
+
+    if [ ! -f .env ]; then
+        echo -e "\033[0;31m\nPlease create .env file\033[0m\n"
+        echo "See https://github.com/spotikum/bascp/blob/master/.env.example"
+        cd $current_dir
+        exit 1
+    fi
+
+    source .env
+    echo -e "\n[$(date)]" >> backup.log
+    rsync -avz -e "sshpass -p $password ssh -p $port" $username@$remote:$workspace . >> backup.log
+
+    if [ ! $? -eq 0 ]; then
+        echo -e "\033[0;31m\nBackup failed\033[0m\n"
+        cd $current_dir
+        exit 1
+    fi
+
+    echo -e "\033[0;32mDone backup from host:\033[0m$remote\033[0;32m at remote:\033[0m$workspace\033[0;32m to local:\033[0m$arg\033[0m"
     cd $current_dir
-    exit 1
-fi
-
-source .env
-rsync -avz -e "sshpass -p $password ssh -p $port" $username@$remote:$workspace .
-
-if [ ! $? -eq 0 ]; then
-    echo -e "\033[0;31m\nBackup failed\033[0m\n"
-    cd $current_dir
-    exit 1
-fi
-
-echo -e "\033[0;32m\nDone backup from host:$remote at remote:$workspace to local:$1\033[0m\n"
-cd $current_dir
+done
